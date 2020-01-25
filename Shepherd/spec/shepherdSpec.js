f9f2cbcd76ba3d8610db682224a28f1009178175
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-25 15:35:22 +0000

    A shepherd can herd sheep into the pen

--- Shepherd/spec/shepherdSpec.js
+++ Shepherd/spec/shepherdSpec.js
@@ -2,9 +2,18 @@
 
 describe('Shepherd', function(){
   var shepherd;
+  var sheep;
 
   it ('the pen has no sheep by default', function() {
     shepherd = new Shepherd();
     expect(shepherd.pen).toEqual([]);
+  });
+
+  it('can count the number of sheep in the pen', function() {
+    shepherd = new Shepherd();
+    sheep = new Sheep();
+    shepherd.herd(sheep)
+    shepherd.herd(sheep)
+    expect(shepherd.pen).toEqual([sheep, sheep]);
   });
 });
--- Shepherd/src/shepherd.js
+++ Shepherd/src/shepherd.js
@@ -4,3 +4,8 @@ function Shepherd() {
   this.pen = [];
 
 };
+
+Shepherd.prototype.herd = function(sheep) {
+  this.pen.push(sheep);
+  return "Sheep has been herded into the pen"
+}
